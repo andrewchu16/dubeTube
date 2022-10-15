2a6e818b51c8d3a7843da96942cea45f72589753
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2022-10-15 04:57:16 -0400

    search

--- static/js/search.js
+++ static/js/search.js
@@ -1,5 +1,10 @@
 function submitSearch() {
-    query = document.getElementsByName('query').item(0).value.trim();
+    let query = document.getElementsByName('query').item(0).value.trim();
+
+    if (query.length > 5) {
+        query = query.substring(0, 5);
+    }
+    
     if (query) {
         document.getElementById('search-bar').submit();
     }
