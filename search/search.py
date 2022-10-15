702cb8e5332494015117f57a5e77e0a111f9ec3f
withercraft303 <lsntao.withercraft303@gmail.com>
2022-10-15 02:19:51 -0400

    wow we can search now

---
+++ search/search.py
@@ -0,0 +1,29 @@
+def SearchText(query, text):
+    n = len(query)+1
+    m = len(text)+1
+
+    diff_array = []
+
+    for i in range(n):
+        diff_array.append([])
+        for j in range(m):
+            diff_array[i].append(0)
+    
+    for i in range(n):
+        diff_array[i][0] = i
+    for j in range(m):
+        diff_array[0][j] = j
+
+    for i in range(1, n):
+        for j in range(1, m):
+            k = min(diff_array[i-1][j-1], diff_array[i][j-1], diff_array[i-1][j]) + 1
+            if (query[i-1] == text[j-1]):
+                k -= 1
+            diff_array[i][j] = k
+
+    return diff_array[n-1][m-1]
+
+def SearchTextRatio(query, text):
+    return len(query)/(len(text) - SearchText(query, text))
+
+print(SearchTextRatio(input(), "i love cats"))
