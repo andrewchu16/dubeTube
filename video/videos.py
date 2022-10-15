b31f77a92ebc909162984b2e7190d6ceccb153b1
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-10-15 02:15:41 -0400

    more video methods

--- video/videos.py
+++ video/videos.py
@@ -1,6 +1,5 @@
 
-
-video = []
+videos = []
 
 class Video:
     def __init__(self: object, video_id: str, thumbnail: str, extension: str, title: str, author: str, date: str, tags: list):
@@ -16,4 +15,28 @@ class Video:
         self.tags = tags
 
 def add_video(video_id: str, thumbnail: str, extension: str, title: str, author: str, date: str):
-    video.append(Video(video_id, thumbnail, extension, title, author, date))
+    videos.append(Video(video_id, thumbnail, extension, title, author, date))
+
+def find_by_id(v_id: int):
+    for video in videos:
+        if video.id == v_id:
+            return video
+
+def get_all_videos() -> list:
+    return videos
+
+def convert_to_dict(v_id: int) -> dict:
+    video = find_by_id(v_id)
+    return_dict = {
+        "id": video.id,
+        "thumbnail": video.thumbnail,
+        "extension": video.extension,
+        "title": video.title,
+        "author": video.author,
+        "date_posted": video.date_posted,
+        "views": video.views
+        "likes": video.likes,
+        "dislikes": video.dislikes,
+        "tags": video.tags
+    }
+    return return_dict
