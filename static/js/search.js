9937bea75612b8bcb1d48d4a0cc77637870fa88e
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2022-10-15 09:21:00 -0400

    header styling

--- static/js/search.js
+++ static/js/search.js
@@ -1,8 +1,10 @@
-function submitSearch() {
+function submitSearch(e) {
+    e.preventDefault();
     let query = document.getElementsByName('query').item(0).value.trim();
 
     if (query.length > 5) {
         query = query.substring(0, 5);
+        document.getElementsByName('query').item(0).value = query.substring(0, 5);
     }
     
     if (query) {
